8540a94fe2ae525d7c1043bccfc8e45e825ec17a
shuvraj07 <54624648+shuvraj07@users.noreply.github.com>
2025-09-06 20:17:03 +0545

    v5

--- app/room/[id]/page.tsx
+++ app/room/[id]/page.tsx
@@ -1,6 +1,6 @@
 import SingleRoom from "@/app/ui/singleRoom/room";
 
-export default async function RoomPage({ params }: { params: { id: string } }) {
+export default async function RoomPage() {
   // params.id is directly accessible here, no need to await if just destructured like this,
   // but Next.js requires the function to be async to handle some internal async operations.
 
